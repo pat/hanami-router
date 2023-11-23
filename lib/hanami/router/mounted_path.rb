f3855ead02ff186a716f991c5a387e37f441bf33
Pat Allan <pat@freelancing-gods.com>
2023-11-24 00:54:51 +1100

    fix PATH_INFO/SCRIPT_NAME for root mounts
    If a mounted Rack app is at the root of the Hanami app, it should keep
    the leading slash in PATH_INFO, and the SCRIPT_INFO value should be
    blank. As per the Rack spec on SCRIPT_INFO:
    
    > This may be an empty string, if the application corresponds to the
    > “root” of the server.
    https://github.com/rack/rack/blob/main/SPEC.rdoc

--- lib/hanami/router/mounted_path.rb
+++ lib/hanami/router/mounted_path.rb
@@ -11,9 +11,13 @@ module Hanami
       def endpoint_and_params(env)
         return [] unless (match = @prefix.peek_match(env[::Rack::PATH_INFO]))
 
-        env[::Rack::SCRIPT_NAME] = env[::Rack::SCRIPT_NAME].to_s + prefix.to_s
-        env[::Rack::PATH_INFO] = env[::Rack::PATH_INFO].sub(prefix.to_s, EMPTY_STRING)
-        env[::Rack::PATH_INFO] = DEFAULT_PREFIX if env[::Rack::PATH_INFO] == EMPTY_STRING
+        if @prefix.to_s == "/"
+          env[::Rack::SCRIPT_NAME] = EMPTY_STRING
+        else
+          env[::Rack::SCRIPT_NAME] = env[::Rack::SCRIPT_NAME].to_s + @prefix.to_s
+          env[::Rack::PATH_INFO] = env[::Rack::PATH_INFO].sub(@prefix.to_s, EMPTY_STRING)
+          env[::Rack::PATH_INFO] = DEFAULT_PREFIX if env[::Rack::PATH_INFO] == EMPTY_STRING
+        end
 
         [@app, match.named_captures]
       end
